e7fd95a1b2de2006386b900a714926dcb016acf6
JeongMin Woo <minhan_a@naver.com>
2018-11-18 17:22:55 +0900

    master : src/Root.js - wrapping Provider

--- blog-frontend/src/Root.js
+++ blog-frontend/src/Root.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import App from 'component/App';
+import { Provider } from 'react-redux';
+import configure from 'store/configure';
 
+const store = configure();
 class Root extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <App/>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App/>
+        </BrowserRouter>
+    </Provider>
     );
   }
 }
