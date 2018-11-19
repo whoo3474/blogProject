6bf1712518490f1ae328285e6c34a4581cc0efb2
JeongMin Woo <minhan_a@naver.com>
2018-11-19 23:55:05 +0900

    master : create PageTemplate/PageTemplate.js

---
+++ blog-frontend/src/component/common/PageTemplate/PageTemplate.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './PageTemplate.scss';
+import className from 'classnames/bind';
+
+const PageTemplate = () => {
+  return (
+    <div className={cx('page-template')}>
+      PageTemplate
+    </div>
+  );
+};
+
+export default PageTemplate;
