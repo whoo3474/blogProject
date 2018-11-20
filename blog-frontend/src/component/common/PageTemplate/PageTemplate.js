d962659fd4ed5a153d8860354510de61a89c6be4
JeongMin Woo <minhan_a@naver.com>
2018-11-20 22:26:03 +0900

    master : create PageTemplate/index.js

--- blog-frontend/src/component/common/PageTemplate/PageTemplate.js
+++ blog-frontend/src/component/common/PageTemplate/PageTemplate.js
@@ -2,6 +2,8 @@ import React from 'react';
 import styles from './PageTemplate.scss';
 import className from 'classnames/bind';
 
+const cx = className.bind(styles);
+
 const PageTemplate = () => {
   return (
     <div className={cx('page-template')}>
