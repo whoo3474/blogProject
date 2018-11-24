55511dfa55aa96363b5f31fb3e9bd752279edd7b
JeongMin Woo <minhan_a@naver.com>
2018-11-24 13:48:18 +0900

    master : import Header/Header.js in PageTemplate.js

--- blog-frontend/src/component/common/PageTemplate/PageTemplate.js
+++ blog-frontend/src/component/common/PageTemplate/PageTemplate.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import styles from './PageTemplate.scss';
 import className from 'classnames/bind';
+import Header from 'components/common/Header';
 
 const cx = className.bind(styles);
 
 const PageTemplate = () => {
   return (
     <div className={cx('page-template')}>
-      PageTemplate
+      <Header />
     </div>
   );
 };
