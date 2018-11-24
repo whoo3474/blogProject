9f19ec7bb5104e72d0d9340bda523ed8eaa9ff1e
JeongMin Woo <minhan_a@naver.com>
2018-11-24 13:43:40 +0900

    master : add react-router-dom Header/Header.js

--- blog-frontend/src/component/common/Header/Header.js
+++ blog-frontend/src/component/common/Header/Header.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import styles from './Header.scss';
+import classNames from 'classnames/bind';
+import { Link } from 'react-router-dom';
 
-const Header = () => {
-  return (
-    <div>
-      Header
+const cx = classNames.bind(styles);
+
+const Header = () => (
+  <header className={cx('header')}>
+    <div className={cx('header-content')}>
+      <div className={cx('brand')}>
+      <Link to="/">reactBlog</Link>
+      </div>
+      <div className={cx('right')}>
+      {}
+      오른쪽
+      </div>
     </div>
-  );
-};
+  </header>
+);
 
 export default Header;
