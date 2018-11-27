fd09aff693f9494ce36db1c309fb521df691bb50
JeongMin Woo <minhan_a@naver.com>
2018-11-28 01:16:55 +0900

    magser : backend - create posts/posts.ctrl.js

---
+++ blog-backend/src/api/posts/posts.ctrl.js
@@ -0,0 +1,107 @@
+let postId = 1;
+
+const posts = [
+  {
+    id: 1,
+    title: '제목',
+    body: '내용'
+  }
+];
+
+// 포스트 생성
+exports.write = (ctx) => {
+  const {
+    title,
+    body
+  } = ctx.request.body;
+
+  postId += 1;
+
+  const post = { id: postId, title, body };
+  posts.push(post);
+
+  ctx.body = post;
+};
+
+// 포스트 목록 조회
+exports.list = (ctx) => {
+  ctx.body = posts;
+};
+
+// 특정 포스트 조회
+
+exports.read = (ctx) => {
+  const {id} = ctx.params;
+
+  const post = posts.find(p => p.idtoString() === id);
+
+  if(!post) {
+    ctx.status = 404;
+    ctx.body = {
+      message : '포스트가 존재하지 않습니다'
+    };
+    return;
+  }
+
+  ctx.body = post;
+};
+
+// 특정 포스트 제거
+exports.remove = (ctx) => {
+  const {id} = ctx.params;
+
+  const index = posts.findIndex(p => p.id.toString() === id);
+
+  if(index === -1) {
+    ctx.status = 404;
+    ctx.body = {
+      message : '포스트가 존재하지 않습니다'
+    };
+    return;
+  }
+
+  posts.splice(index,1);
+  ctx.status = 204;
+};
+
+// 포스트 수정(교체)
+exports.replace = (ctx) => {
+  const {id} = ctx.params;
+
+  const index = posts.findIndex(p=>p.id.toString() === id);
+
+  if(index === -1){
+    ctx.status = 404;
+    ctx.body = {
+      message : '포스트가 존재하지 않습니다'
+    };
+    return;
+  }
+
+  posts[index] = {
+    id,
+    ...ctx.request.body
+  };
+  ctx.body = posts[index];
+};
+
+// 포스트 수정(특정필드 변경)
+exports.update = (ctx) => {
+  const {id} = ctx.params;
+
+  const index = posts.findIndex(p=>p.id.toString()===id);
+
+  if(index === -1){
+    ctx.status = 404;
+    ctx.body = {
+      message : '포스트가 존재하지 않습니다'
+    };
+    return;
+  }
+
+  posts[index] = {
+    ...posts[index],
+    ...ctx.request.body
+  };
+  ctx.body = posts[index];
+}
